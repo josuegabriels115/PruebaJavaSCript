7710e5d77a41ffdc62757469951dd12181aa660c
Gabriel Sanchez <josue.gabriels115@gmail.com>
2025-04-24 16:01:15 -0500

    esta es la prueba para pasante de desarrollo full stack

---
+++ prueba.js
@@ -0,0 +1,22 @@
+
+//obtendrías solo uno de los nombres y en mayúscula
+const personas = [
+    { nombre: "Ana", edad: 25 },
+    { nombre: "Luis", edad: 30 },
+    { nombre: "María", edad: 22 }
+  ];
+  
+  /*defino la funcion con dos parametros, el nombre del arreglo y el nombre a buscar*/ 
+  function buscarPersonas(array, nameFinded) { 
+
+      const persona = array.find(p => p.nombre === nameFinded)/* defino una const donde va  a buscar  en el array 
+                                                                el parametro por el metodo find 
+                                                                y lo va almacenar */
+
+
+      return persona ? persona.nombre.toUpperCase() : "No encontrado"; /* Defino una condicional donde si persona == True 
+                                                                         entonces me devuelva en mayuscula y si no, entonces
+                                                                         devuelve el mensaje No encontrado*/
+  }
+  const resultado = buscarPersonas(personas,"Ana");
+  console.log(resultado);
